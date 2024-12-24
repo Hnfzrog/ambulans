057581924fbfba0a96ccbfbc197acd789b900584
Hnfzrog <younghans532@gmail.com>
2024-12-25 00:40:52 +0700

    update filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -65,7 +65,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        public_path('/public_html/storage') => storage_path('app/public'),
     ],
 
 ];
